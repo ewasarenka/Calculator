58ab2c82057f4a294d6ac4adfaf02bb7ec0a90cc
ewasarenka <kiwinaolx@gmail.com>
2023-01-07 02:36:52 +0100

    Fixed DEL key

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
-
-/* FUNCTIONS */
+/*      FUNCTIONS       */
 const add = (a, b) => Number(a) + Number(b);
 const subtract = (a, b) => (a - b);
 const multiply = (...arr) => arr.reduce((total, number) => { 
     return total * number;
 }, 1);
 const divide = (a, b) => a / b;
-const operate = (operator,num1,num2) => operator(num1,num2);
-const changeNumSign = (a) => -a;
+const changeNumSign = (a) => {
+    return (
+        a === '-'? '':
+        a === '' || a === undefined? '-':
+        String(-Number(a)));
+}
 
-/* Calculator events */
+const operate = (operator,num1,num2) => operator(num1,num2);
+const calculations = (sign,op) => {
+    while (operators.indexOf(sign)>=0) {
+        let j = operators.indexOf(sign);
+        numbers[j] = operate(op,numbers[j],numbers[j+1])
+        operators.splice(j,1);
+        numbers.splice(j+1,1);
+    }
+}
+
+const display = () => {
+    let view = '';
+    if (operators == undefined) {
+        view = numbers[0];
+    } else {
+        for (let i = 0; i < operators.length; i++) view += numbers[i] + operators[i];
+        if (numbers.length > operators.length) view += numbers[i];
+    }
+    OperationView.textContent = view;
+}
+
+/*      Calculator events       */
 
 let numbers = [];
 let operators = [];
@@ -17,79 +41,28 @@ let i = 0;
 
 const OperationView = document.querySelector('.operations');
 const ResultView = document.querySelector('.result');
-
 const Buttons = document.querySelectorAll('button');
 Buttons.forEach(btn => {
     
     btn.addEventListener('click', event => {
 
-        if (btn.textContent == '=') {
-            let calc = operators.length;
-            let result;
-
-            while (operators.indexOf('x')>=0) {
-                let j = operators.indexOf('x');
-                numbers[j] = operate(multiply,numbers[j],numbers[j+1])
-                operators.splice(j,1);
-                numbers.splice(j+1,1);
-                console.log(operators,numbers);
-            }
-
-            while (operators.indexOf('÷')>=0) {
-                let j = operators.indexOf('÷');
-                numbers[j] = operate(divide,numbers[j],numbers[j+1])
-                operators.splice(j,1);
-                numbers.splice(j+1,1);
-                console.log(operators,numbers);
-            }
-
-            while (operators.indexOf('-')>=0) {
-                let j = operators.indexOf('-');
-                numbers[j] = operate(subtract,numbers[j],numbers[j+1])
-                operators.splice(j,1);
-                numbers.splice(j+1,1);
-                console.log(operators,numbers);
-            }
-
-            while (operators.indexOf('+')>=0) {
-                let j = operators.indexOf('+');
-                numbers[j] = operate(add,numbers[j],numbers[j+1])
-                operators.splice(j,1);
-                numbers.splice(j+1,1);
-                console.log(operators,numbers);
-            }
-                
-            OperationView.textContent = numbers[0];
-            ResultView.textContent = numbers[0];
-            operators = [];
-            i=0;
-            
-        } else if (btn.className == 'digit') {
-            OperationView.textContent += btn.textContent;
+        if (btn.className == 'digit') {
             if (numbers[i] == undefined) numbers[i] = ''; 
             numbers[i] += btn.textContent;  
+            display()
 
         } else if (btn.className == 'fun') {
-            OperationView.textContent += btn.textContent;
-            if (operators[i] == undefined) operators[i] = ''; 
-            operators[i] = btn.textContent;
-            i++  
-
-        /* } 
-        
-        // NEED FIX
-        else if (btn.textContent == 'DEL'  ) {
-            let string = OperationView.textContent;
-            console.log(string.charAt(-1));
-            if ( (numbers.length > operators.length && ResultView!='') || (ResultView=='') ) {
-                console.log('yes')
-                numbers[i].splice(numbers[i].length,1);
+            if (numbers.length == operators.length) {
+                operators[i-1] = btn.textContent;
             } else {
-                console.log("no")
+                operators[i] = btn.textContent;
+                i++;
             }
-            OperationView.textContent = string.slice(0, -1);
-            operators = [];
-            i=0; */
+            display();
+
+        } else if (btn.className == 'sign') {
+            numbers[i] = changeNumSign(numbers[i]);
+            display();
 
         } else if (btn.textContent == 'AC'  ) {
             OperationView.textContent = '';
@@ -98,6 +71,30 @@ Buttons.forEach(btn => {
             operators = [];
             i=0;
         }
+        
+        // NEED FIX
+        else if (btn.textContent == 'DEL'  ) {
+            if (numbers.length > operators.length && numbers[i] != '') {
+                numbers[i] = numbers[i].slice(0,numbers[i].length-1)
+            } else {
+                operators.pop();
+                numbers.pop();
+                i--;
+                console.log("no");
+            }
+            display();
+            
+        // Calculations
+        } else if (btn.textContent == '=') {
+            calculations('&#215',multiply);
+            calculations('÷',divide);
+            calculations('-',subtract);
+            calculations('+',add);
+            ResultView.textContent = numbers[0];
+            operators = [];
+            i=0;
+            display();                  
+        } 
 
         console.log(numbers,operators); 
 
